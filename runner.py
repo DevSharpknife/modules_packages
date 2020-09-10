f612424fa97f02b61d7e0b399c3d2a15a66095c9
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-10 15:49:37 +0100

    created app.py and moved functions there

--- runner.py
+++ runner.py
@@ -1,4 +1,7 @@
-from calculator import add, subtract, multiply, divide
+## text. before file shows filepath to find
+from modules.calculator import *
+## from calculator import add, subtract, multiply, divide
+## import calculator
 
 print(add(3, 5))
 print(multiply(9, 12))
